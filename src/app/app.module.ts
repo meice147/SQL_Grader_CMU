2a35705c5f9913a030374685e11595d8b69f8ef4
Unknown <fofanza1@gmail.com>
2017-04-09 15:52:39 +0700

    add express and material design angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import 'hammerjs';
+
+
 
 import { AppComponent } from './app.component';
 
@@ -10,6 +15,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    NoopAnimationsModule,
+    BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
     HttpModule
